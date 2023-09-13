b4dd0d17a8909454bff16c4ab4c8363f2d60934b
Marison <marison.sol@outlook.com>
2023-09-13 14:21:17 +0200

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <title>Search and retrieve data</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="witdh=device=witdh, initial-scale=1.0">
+    </head>
+    <body>
+        <form action="start.php" method="post">
+            ID to search: <input type="text" name="id"><br><br>
+            First Name: <input type="text" name="fname"><br><br>
+            Last Name: <input type="text" name="lname"><br><br>
+        </form>
+    </body>
+</html>
