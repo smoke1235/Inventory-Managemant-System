a19467ad9ffd2f2b78fe29fbc3cd3b9e37ed8026
Marison <marison.sol@outlook.com>
2023-09-18 09:26:06 +0200

    added database credentials to prifile php file

--- Login/profile.php
+++ Login/profile.php
@@ -4,4 +4,9 @@ if (!isset($_SESSION['loggedin'])) {
     header('Location: index.html');
     exit
 }
+
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'inventoryManager';
 ?>
