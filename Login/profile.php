52fda5c2ff08920ac306676a97415d68f5c404dd
Marison <marison.sol@outlook.com>
2023-09-18 11:19:30 +0200

    fixed php errors

--- Login/profile.php
+++ Login/profile.php
@@ -3,7 +3,7 @@ session_start();
 
 if (!isset($_SESSION['loggedin'])) {
     header('Location: index.html');
-    exit
+    exit;
 }
 
 $DATABASE_HOST = 'localhost';
@@ -12,7 +12,7 @@ $DATABASE_PASS = '';
 $DATABASE_NAME = 'inventoryManager';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error())
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
 $stmt = $con->prepare('SELECT password, email FROM users WHERE id = ?');
