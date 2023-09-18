a9bf371f30a07c5a1fef67804ec78bc76aa0f878
Marison <marison.sol@outlook.com>
2023-09-18 11:11:34 +0200

    added html and css file

--- Login/profile.php
+++ Login/profile.php
@@ -23,3 +23,18 @@ $stmt->bind_result($password, $email);
 $stmt->fetch();
 $stmt->close();
 ?>
+
+<!doctype <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Profile Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="profile.css" rel="stylesheet">
+    </head>
+    <body class="loggedin">
+        <nav class="navtop">
+            
+        </nav>
+    </body>
+</html>
