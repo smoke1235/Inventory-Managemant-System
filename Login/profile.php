abd83b784371a34b1ba6b67a22fdf4e9c7b30012
Marison <marison.sol@outlook.com>
2023-09-18 09:33:14 +0200

    made statement to get info from database

--- Login/profile.php
+++ Login/profile.php
@@ -13,4 +13,6 @@ $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error())
 }
+
+$stmt = $con->prepare('SELECT password, email FROM users WHERE id = ?');
 ?>
