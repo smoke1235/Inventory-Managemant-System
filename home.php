982aa5c6af1dab8019bd0d212b364311f7178907
Marison <marison.sol@outlook.com>
2023-09-21 14:22:51 +0200

    added side bar

--- home.php
+++ home.php
@@ -12,22 +12,30 @@ if ( !isset($_SESSION['loggedin']) ) {
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Home Page</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home Page</title>
+        <meta name="discription" content="">
         <link href="CSS/main.css" rel="stylesheet">
     </head>
     <body>
-        <nav class="navtop">
+        <nav aria-label="nav-top" class="nav-top">
             <a href="home.php"><h1>Website Title</h1></a>
             <ul>
-                <li><a href="profile.php">Profile</a></li>
-                <li><a href="logout.php">Logout</a></li>
+                <li><a href="PHP/home.php">Home</a></li>
+                <li><a href="PHP/profile.php">Profile</a></li>
+                <li><a href="PHP/logout.php">Logout</a></li>
             </ul>
         </nav>
-        <div class="content">
-            <h2>Home Page</h2>
-            <p>Welcome, <?=$_SESSION['name']?>!</p>
-        </div>
+        <nav aria-label="nav-left" class="nav-left">
+            <ul>
+                <li><a href="">Dashboard</a></li>
+                <li><a href="">Customer</a></li>
+                <li><a href="">Supplier</a></li>
+                <li><a href="">Catergory</a></li>
+                <li><a href="">Stock</a></li>
+                <li><a href="">Sells</a></li>
+            </ul>
+        </nav>
     </body>
 </html>
