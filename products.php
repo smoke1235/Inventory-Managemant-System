98f263ec62a80f24c27a61c087d1b2dbc29b7438
Marison <marison.sol@outlook.com>
2023-09-25 09:33:58 +0200

    Update products.php

--- products.php
+++ products.php
@@ -7,6 +7,17 @@ if ( !isset($_SESSION['loggedin']) ) {
     header('Location: index.html');
     exit;
 }
+
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'inventoryManager';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
 ?>
 
 <!DOCTYPE html>
