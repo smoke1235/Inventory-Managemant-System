665bc74a859f28d1a135dbf5abbb9c96f52ab75a
Marison <marison.sol@outlook.com>
2023-09-27 12:24:42 +0200

    Update insertProduct.php

--- insertProduct.php
+++ insertProduct.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 session_start();
-if ( !isset($_SESSION['loggedin']) ) {
+if (!isset($_SESSION['loggedin'])) {
     header('Location: index.html');
     exit;
 }
@@ -20,52 +20,57 @@ if (mysqli_connect_errno()) {
 
 $productName = $_REQUEST['productName'];
 $quantity = $_REQUEST['quantity'];
+$int = (int)$quantity;
 
-$sql =  "INSERT INTO products (productName, quantity) VALUES ('$productName', '$quantity')";
+$sql = "INSERT INTO products (productName, quantity) VALUES ('$productName', '$int')";
 if (mysqli_query($con, $sql)) {
     echo "insert succesfull";
 } else {
     echo "insert unsuccesfull";
 }
-
 $con->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add New Product</title>
-    <link rel="stylesheet" href="Assets/SCSS/main.scss">
+    <link rel="stylesheet" href="SCSS/main.scss">
 </head>
+
 <body>
-<nav aria-label="nav-top" class="nav-top">
-            <a href="home.php"><h1>Website Title</h1></a>
-            <ul>
-                <li><a href="profile.php">Profile</a></li>
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-        </nav>
-        <nav aria-label="nav-left" class="nav-left">
-            <ul>
-                <li><a href="home.php">Dashboard</a></li>
-                <li><a href="products.php">Products</a></li>
-                <li><a href="stock.php">Stock</a></li>
-                <li><a href="orders.php">Orders</a></li>
-                <li><a href="customers.php">Customers</a></li>
-                <li><a href="suppliers.php">Suppliers</a></li>
-            </ul>
-        </nav>
-        <h1>Add New Product</h1>
-        <form action="insertProduct.php" method="POST">
-            <label for="productName" name="productName">Product Name *:</label>
-            <input type="text" name="productName" id="productName" placeholder="Product Name" required>
+    <nav aria-label="nav-top" class="nav-top">
+        <a href="home.php">
+            <h1>Website Title</h1>
+        </a>
+        <ul>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </nav>
+    <nav aria-label="nav-left" class="nav-left">
+        <ul>
+            <li><a href="home.php">Dashboard</a></li>
+            <li><a href="products.php">Products</a></li>
+            <li><a href="stock.php">Stock</a></li>
+            <li><a href="orders.php">Orders</a></li>
+            <li><a href="customers.php">Customers</a></li>
+            <li><a href="suppliers.php">Suppliers</a></li>
+        </ul>
+    </nav>
+    <h1>Add New Product</h1>
+    <form action="insertProduct.php" method="POST">
+        <label for="productName" name="productName">Product Name *:</label>
+        <input type="text" name="productName" id="productName" placeholder="Product Name" required>
 
-            <label for="quantity" name="quantity">Quantity:</label>
-            <input type="number" name="quantity" id="quantity" placeholder="0">
+        <label for="quantity" name="quantity">Quantity:</label>
+        <input type="number" name="quantity" id="quantity" placeholder="0">
 
-            <input type="submit" value="Submit">
-        </form>
+        <input type="submit" value="Submit">
+    </form>
 </body>
+
 </html>
