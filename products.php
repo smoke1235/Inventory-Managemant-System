76babd6b60b0234229b17061ada13662bc46c71d
Marison <marison.sol@outlook.com>
2023-10-06 11:52:50 +0200

    remade navbar

--- products.php
+++ products.php
@@ -39,26 +39,7 @@ $con->close();
 </head>
 
 <body>
-    <nav aria-label="nav-top" class="nav-top">
-        <a href="home.php">
-            <h1>Website Title</h1>
-        </a>
-        <ul>
-            <li><a href="profile.php">Profile</a></li>
-            <li><a href="logout.php">Logout</a></li>
-        </ul>
-    </nav>
-    <nav aria-label="nav-left" class="nav-left">
-        <ul>
-            <li><a href="home.php">Dashboard</a></li>
-            <li><a href="products.php">Products</a></li>
-            <li><a href="stock.php">Stock</a></li>
-            <li><a href="orders.php">Orders</a></li>
-            <li><a href="customers.php">Customers</a></li>
-            <li><a href="suppliers.php">Suppliers</a></li>
-        </ul>
-    </nav>
-
+    <?php include_once 'navbar.php'; ?>
     <h1>Products</h1>
     <a href="insertProduct.php">Add</a>
     <table id="myTable">
