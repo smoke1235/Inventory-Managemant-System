f7911ed57a0a8d3d993890b6aa3856eaa0ccfa3f
Marison <marison.sol@outlook.com>
2023-11-06 16:28:36 +0100

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,5 @@
 const sidebar = document.querySelector('.sidebar');
-const mainContent = document.querySelector('.main-content');
-document.querySelector('button').onclick = function () {
-  sidebar.classList.toggle('sidebar_closed');
+const mainContent = document.querySelector('.main-content');document.querySelector('button').onclick = function () {
+  sidebar.classList.toggle('sidebar_small');
   mainContent.classList.toggle('main-content_large')
 }
