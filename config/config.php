2c18a708fb14db1f9e5a463867bc5e54d7c17492
Marison <marison.sol@outlook.com>
2023-11-06 09:09:59 +0100

    Delete config/config.php

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "inventoryManager";
-$dsn = "mysql:host=$host;dbname=$dbname";
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-];
