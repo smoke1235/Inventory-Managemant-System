12e2de353fc3f8b8e358020cfde25a929e9b39e1
Marison <marison.sol@outlook.com>
2023-11-13 11:11:22 +0100

    updated search.php & some styling changes

--- include/navbar.php
+++ include/navbar.php
@@ -25,7 +25,7 @@
 </nav>
 
 <nav class="sidebar" id="sidebar" aria-label="">
-<img src="../assets/img/TEEUWEN_logo_transparant-min.png" alt="Company logo" height="75" width="250">
+<img src="../assets/img/TEEUWEN_logo_transparant-min.png" alt="Company logo" height="33%" width="100%">
     <ul>
         <li><a href="dashboard.php">Dashboard</a></li>
         <li><a href="products.php">Products</a></li>
--- public/search.php
+++ public/search.php
@@ -20,7 +20,69 @@ require_once '../config/connect.php';
 </head>
 
 <body>
+    <div class="dashboard-container">
+        <?php include_once '../include/navbar.php'; ?>
+        <main class="main-content">
+            <h1>Search Items</h1>
+            <div class="seacrh-container">
+                <form class="search-form">
+                    <input type="text" name="item-search" placeholder="Search for products, Customers & Suppliers.">
+                    <input type="submit">
+                </form>
+            </div>
+            <div class="result-container">
+                <div class="result-product">
+                    <h2>Products results</h2>
+                    <div class="results-table">
+                        <table aria-label="">
+                            <thead>
+                                <th>No.</th>
+                                <th>Name</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                            </thead>
+                            <tbody>
 
+                            </tbody>
+                        </table>
+                    </div>
+                    <hr>
+                </div>
+                <div class="result-customer">
+                    <h2>Customer results</h2>
+                    <div class="results-table">
+                        <table aria-label="">
+                            <thead>
+                                <th>Name</th>
+                                <th>Phone Number</th>
+                                <th>Quantity</th>
+                                <th>Email</th>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="result-supplier">
+                    <h2>Supplier results</h2>
+                    <div class="results-table">
+                        <table aria-label="">
+                            <thead>
+                                <th>Name.</th>
+                                <th>Phoen Number</th>
+                                <th>Email</th>
+                                <th>Price</th>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
 </body>
 
 </html>
