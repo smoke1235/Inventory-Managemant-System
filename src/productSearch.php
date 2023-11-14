9f80a3d4a14e737fe1dafc15fec2eb2a5001de8b
Marison <marison.sol@outlook.com>
2023-11-14 16:23:21 +0100

    Update productSearch.php

--- src/productSearch.php
+++ src/productSearch.php
@@ -6,3 +6,5 @@ FROM products INNER JOIN suppliers ON products.supplier_id=suppliers.id
 WHERE (products.product_name LIKE '$search%' OR name LIKE '$search%')");
 $stmt->execute(["%" . $_POST['search'] . "%"]);
 $results = $stmt->fetchAll();
+
+mysqli_connect($con)->close();
