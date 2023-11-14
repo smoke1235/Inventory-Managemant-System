c7410e18da2f9c389f7bf7dfdf784694d2ecbe45
Marison <marison.sol@outlook.com>
2023-11-14 16:17:03 +0100

    Update productSearch.php

--- src/productSearch.php
+++ src/productSearch.php
@@ -4,4 +4,5 @@ require_once '../config/connect.php';
 $stmt = $con->prepare("SELECT product_name, name
 FROM products INNER JOIN suppliers ON products.supplier_id=suppliers.id
 WHERE (products.product_name LIKE '$search%' OR name LIKE '$search%')");
-$stmt->execute()
+$stmt->execute(["%" . $_POST['search'] . "%"]);
+$results = $stmt->fetchAll();
