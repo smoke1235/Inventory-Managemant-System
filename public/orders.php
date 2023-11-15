337757f52566031d40a06ecb7c1bb586a4118aaf
Marison <marison.sol@outlook.com>
2023-11-15 11:41:58 +0100

    Update orders.php

--- public/orders.php
+++ public/orders.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 if (!isset($_SESSION['loggedin'])) {
     header('Location: ../index.php');
@@ -7,3 +6,20 @@ if (!isset($_SESSION['loggedin'])) {
 }
 
 require_once '../config/connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory Manager | Orders</title>
+    <meta name="description" content="">
+    <link rel="stylesheet" href="../assets/CSS/main.css">
+</head>
+<body>
+    <div class="dashboard-container">
+        
+    </div>
+</body>
+</html>
