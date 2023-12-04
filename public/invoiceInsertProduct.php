38d0a18c2d5e9642bf1fff77b3f8e06bd24698c4
Marison <marison.sol@outlook.com>
2023-12-04 17:00:53 +0100

    styling and function fixing

--- public/invoiceInsertProduct.php
+++ public/invoiceInsertProduct.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iventory Manager | Add product for invoice</title>
     <link rel="stylesheet" href="../assets/CSS/popup.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="../assets/js/showProductResult.js"></script>
 </head>
 
@@ -15,9 +16,9 @@
     <div class="container">
         <div class="input-container">
             <h1>Search Product</h1>
-            <input type="text" name="search" id="search" placeholder="Search" onkeyup="load_data(this.value);">
+            <input type="text" name="search" id="search" placeholder="Search">
             <div class="results-container">
-                <table id="">
+                <table aria-label="">
                     <thead>
                         <tr>
                             <th id="number">No.</th>
@@ -27,7 +28,7 @@
                             <th id="price">Price</th>
                         </tr>
                     </thead>
-                    <tbody id="post_data"></tbody>
+                    <tbody id="result"></tbody>
                 </table>
             </div>
         </div>
--- src/liveSearch.php
+++ src/liveSearch.php
@@ -1,63 +1,32 @@
 <?php
-if (isset($_POST["query"])) {
-    require_once '../config/connect.php';
+require_once '../config/connect.php';
 
-    $data = array();
-
-    if ($_POST["query"] != '') {
-        $condition = preg_replace('/[^A-Za-z0-9\- ]/', '', $_POST["query"]);
-        $condition = trim($condition);
-        $condition = str_replace(" ", "%", $condition);
-
-        $sample_data = array(
-            ':id'                   => '%' . $condition . '%',
-            ':product_name'         => '%' . $condition . '%',
-            ':product_description'  => '%' . $condition . '%',
-            ':product_quantity'     => '%' . $condition . '%',
-            ':product_price'        => '%' . $condition . '%'
-        );
-
-        $quary  = "SELECT * FROM products
-        WHERE id LIKE :id
-        OR product_description LIKE :product_description
-        OR product_quantity LIKE :product_quantity
-        OR product_price LIKE :product_price
-        ORDER BY id DESC";
-
-        $stmt = $con->prepare($quary);
-        $stmt->execute($sample_data);
-
-        $result = $stmt->fetchAll();
-
-        $replace_array_1 = explode("%", $condition);
-
-        foreach($replace_array_1 as $row_data) {
-            $replace_array_2[] = '<span>' . $row_data . '</span>';
-        }
-
-        foreach($result as $row) {
-            $data[] = array(
-                'id'                        => str_ireplace($replace_array_1, $replace_array_2, $row["id"]),
-                'product_name'              => str_ireplace($replace_array_1, $replace_array_2, $row["product_name"]),
-                'product_description'       => str_ireplace($replace_array_1, $replace_array_2, $row["product_description"]),
-                'product_quantity'          => str_ireplace($replace_array_1, $replace_array_2, $row["product_quantity"]),
-                'product_price'             => str_ireplace($replace_array_1, $replace_array_2, $row["product_price"])
-            );
-        }
-    } else {
-        $quary = "SELECT * FROM products ORDER BY id";
-
-        $result = $con->query($quary);
-
-        foreach($result as $row) {
-            $data[] = array(
-                'id'                        => $row["id"],
-                'product_name'              => $row["product_name"],
-                'product_description'       => $row["product_description"],
-                'product_quantity'          => $row["product_quantity"],
-                'product_price'             => $row["product_price"]
-            );
-        }
+$return = '';
+if(isset($_POST["query"])) {
+    $search = mysqli_real_escape_string($con, $_POST["query"]);
+    $query = "SELECT * FROM products
+    WHERE id LIKE  '%".$search."%'
+    OR product_name LIKE '%".$search."%'
+    OR product_description LIKE '%".$search."%'
+    OR product_quantity LIKE  '%".$search."%'
+    OR product_price LIKE '%".$search."%'
+    ";
+} else {
+    $query = "SELECT * FROM products";
+}
+$result = mysqli_query($con,$query);
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_array($result)) {
+        $return .= '
+        <tr>
+        <td>'.$row["id"].'</td>
+        <td>'.$row["product_name"].'</td>
+        <td>'.$row["product_description"].'</td>
+        <td>'.$row["product_quantity"].'</td>
+        <td>'.$row["product_price"].'</td>
+        </tr>';
     }
-    echo json_decode($data);
+    echo $return;
+} else {
+    echo 'No results Found.';
 }
