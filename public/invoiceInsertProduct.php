158daa40c606c1d79343e3662ec1cc9be6d3ff29
Marison <marison.sol@outlook.com>
2023-12-04 12:12:20 +0100

    added table and styling

--- public/invoiceInsertProduct.php
+++ public/invoiceInsertProduct.php
@@ -2,18 +2,39 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../assets/CSS/popup.css">
 </head>
+
 <body>
     <div class="container">
         <div class="input-container">
             <h1>Search Product</h1>
             <input type="text" id="">
+            <div class="results-container">
+                <table id="">
+                    <thead>
+                        <tr>
+                            <th id="number">No.</th>
+                            <th id="product">Product</th>
+                            <th id="desc">Description</th>
+                            <th id="qty">Qty</th>
+                            <th id="price">Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            //insert product data
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
