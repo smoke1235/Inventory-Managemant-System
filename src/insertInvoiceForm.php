a05d437ad685a88d191abebb9979e0069efe1f34
Marison <marison.sol@outlook.com>
2023-12-14 12:41:20 +0100

    Update insertInvoiceForm.php

--- src/insertInvoiceForm.php
+++ src/insertInvoiceForm.php
@@ -4,6 +4,7 @@ require_once '../config/connect.php';
 $customer =             $_POST['customer_select'];
 $number =               $_POST['customer_number'];
 $mail =                 $_POST['customer_mail'];
+$status =               $_POST['invoice_status'];
 
 $shipping_name =        $_POST['shipping_name'];
 $shipping_company =     $_POST['shipping_company'];
@@ -11,7 +12,6 @@ $shipping_street =      $_POST['shipping_street'];
 $shipping_postalcode =  $_POST['shipping_postalcode'];
 $shipping_city =        $_POST['shipping_city'];
 $shipping_country =     $_POST['shipping_country'];
-
 $billing_name =         $_POST['billing_name'];
 $billing_company =      $_POST['billing_company'];
 $billing_street =       $_POST['billing_street'];
@@ -19,13 +19,17 @@ $billing_postalcode =   $_POST['billing_postalcode'];
 $billing_city =         $_POST['billing_city'];
 $billing_country =      $_POST['billing_country'];
 
-$status =               $_POST['invoice_status'];
+$sql1 = "INSERT INTO invoices
+(id, status, user_id, number, mail,
+shipping_name, shipping_company, shipping_street, shipping_postalcode, shipping_city, shipping_country,
+billing_name, billing_company, billing_street, billing_postalcode, billing_city, billing_country,
+updated, created)
+VALUES
+('$shipping_name', '$shipping_company', '$shipping_street', '$shipping_postalcode', '$shipping_city', '$shipping_country',
+'$billing_name', '$billing_company', '$billing_street', '$billing_postalcode', '$billing_city', '$billing_country'
+current_timestamp(), current_timestamp()";
 
 $item1 = "";
 $item2 = "";
 
-$sql1 = "";
 $sql2 = "";
-if (mysqli_query($con, $sql1, $sql2)) {
-    echo "<script>alert('There are no fields to generate a report');document.location='admin/ahm/panel'</script>";
-}
