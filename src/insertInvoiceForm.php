f50e5449c89c048ecbb0d9a58127097b478f7552
Marison <marison.sol@outlook.com>
2023-12-18 16:45:07 +0100

    fixed insert to invoice line

--- src/insertInvoiceForm.php
+++ src/insertInvoiceForm.php
@@ -1,9 +1,10 @@
 <?php
+
 require_once '../config/connect.php';
 
 $user =                 $_SESSION['id'];
 $number =               $_POST['customer_number'];
-$mail =                 $_POST['customer_mail'];
+$mail =                 $_POST['customer_email'];
 $status =               $_POST['invoice_status'];
 
 $shipping_name =        $_POST['shipping_name'];
@@ -29,29 +30,34 @@ VALUES
 '$shipping_name', '$shipping_company', '$shipping_street', '$shipping_postalcode', '$shipping_city', '$shipping_country'
 ,'$billing_name', '$billing_company', '$billing_street', '$billing_postalcode', '$billing_city', '$billing_country',
 current_timestamp(), current_timestamp())";
+
 if ($con->query($sql1) === true) {
     $last_id = $con->insert_id;
+} else {
+    echo mysqli_error($con);
 }
 
 $total_products =       count($_POST['product']);
-$total_qty =            count($_POST['invoice_qty']);
+$total_qty =            count($_POST['qty']);
 
-for($i=0;$i<$total_products.$total_qty;$i++) {
+
+for($i=0;$i<$total_products;$i++) {
     $product =      $_POST['product'][$i];
     $qty =          $_POST['qty'][$i];
 
-    $sql2 = mysqli_query($con, "INSERT INTO invoice_line
+    $sql2 = "INSERT INTO invoice_line
     (invoice_id, product_id, quantity)
-    VALUES ('$last_id', '$product', '$qty')");
-    if(!$sql2) {
+    VALUES ($last_id, $product, $qty)";
+    echo $sql2;
+    $sql3 = mysqli_query($con, $sql2);
+    
+
+    if(!$sql3) {
         $error = true;
         $error_msg = $error_msg.mysqli_error($con);
     }
 }
 
-$result = mysqli_query($con, $sql2);
-if ($result) {
+if($sql3) {
     header('location: ../public/invoice.php');
-} else {
-    echo "Error: ".$error_msg;
 }
