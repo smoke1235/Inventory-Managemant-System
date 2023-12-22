3e3645054380bf211ea4ef428a93981a59e497f7
Marison <marison.sol@outlook.com>
2023-12-22 09:18:34 +0100

    start register page

--- public/register.php
+++ public/register.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory Manager | Register</title>
+    <link rel="stylesheet" href="../assets/CSS/register.css">
+    <meta name="description" name="">
+</head>
+<body>
+    <div class="container"></div>
+</body>
+</html>
