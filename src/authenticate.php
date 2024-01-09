fd6d848c468d6a46619ad9894b1de90277e6f255
Marison <marison.sol@outlook.com>
2024-01-09 09:07:28 +0100

    update authrnticatie user and logout

--- src/authenticate.php
+++ src/authenticate.php
@@ -1,22 +1,20 @@
 <?php
-session_start();
-
 require_once '../config/connect.php';
 
-$stmt = $con->prepare('SELECT * FROM users WHERE username = ?');
+$stmt = $con->prepare("SELECT * FROM users WHERE username = ?");
 $stmt->bind_param('s', $_POST['username']);
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
 $stmt->execute();
 $result = $stmt->get_result();
 $user = $result->fetch_assoc();
 
-if ($user && password_verify($_POST['password'], $user['password'])) {
-    $_SESSION['loggedin'] = true;
-    $_SESSION['user_id'] = $user['id'];
-    $_SESSION['name'] = $user['username'];
-    header('Location: ../public/dashboard.php');
-    exit;
+if ($user && password_verify($password, $user['password'])) {
+    echo "JIPPIE";
 } else {
-    echo "Invalid password.";
+    echo "Invalid login.";
 }
 
 $stmt->close();
