81660683f5ea847a2f4fcfec29c1169210f464c1
Marison <marison.sol@outlook.com>
2024-01-10 09:59:44 +0100

    another fix for dynamic header title

--- src/inc/header.php
+++ src/inc/header.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'Inventory Manager' ?></title>
-    <link rel="stylesheet" href="../assets/CSS/register.css">
+    <link rel="stylesheet" href="../assets/CSS/main.css">
     <meta name="description" content="">
 </head>
 <body>
-    <div>
+    <div class="dashboard-container">
         <?php include_once '../src/inc/navbar.php'; ?>
         <main class="main-content">
