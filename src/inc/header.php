b973e1e168eb349edd8a35b4898e673655e2d189
Marison <marison.sol@outlook.com>
2024-01-11 13:24:12 +0100

    added icon

--- src/inc/header.php
+++ src/inc/header.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'Inventory Manager' ?></title>
     <link rel="stylesheet" href="../assets/CSS/main.css">
-    <meta name="description" content="">
+    <link rel="icon" href="">
+    <meta name="description" content="assets/img/checklists.png">
 </head>
 <body>
     <div class="dashboard-container">
