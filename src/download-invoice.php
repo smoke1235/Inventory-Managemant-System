3d5e1b0178b9380d7e2562edf36692350fe172ea
Marison <marison.sol@outlook.com>
2024-01-16 12:09:03 +0100

    update download-invoice

--- src/download-invoice.php
+++ src/download-invoice.php
@@ -1,7 +1,26 @@
 <?php
 require_once('../fpdf186/fpdf.php');
+
+class PDF extends FPDF {
+    function Header() {
+        $this->Image('../assets/img/TEEUWEN_logo_transparant-min.png',10,6,30);
+        $this->SetFont('Arial','B',15);
+        $this->Cell(80);
+        $this->Cell(30,10,'Title',1,0,'C');
+        $this->Ln(20);
+    }
+
+    function Footer() {
+        $this->SetY(-15);
+        $this->SetFont('Arial','I',8);
+        $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    }
+}
+
 $pdf = new FPDF();
+$pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial', 'B', 18);
-$pdf->Cell(60,20,'Hello world');
+$pdf->SetFont('Times', '', 12);
+for ($i=1;$i<=40;$i++)
+    $pdf->Cell(0,10,'Printing line number '.$i,0,1);
 $pdf->output();
