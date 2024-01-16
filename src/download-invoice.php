49b3091ca468671e593d2ed397220ad3d2bc7c1d
Marison <marison.sol@outlook.com>
2024-01-16 11:26:37 +0100

    update download-invoice

--- src/download-invoice.php
+++ src/download-invoice.php
@@ -1,9 +1,4 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-ob_end_clean();
 require_once('../fpdf186/fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage();
