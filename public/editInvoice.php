8183be6248269eef6d98d42c228e314adb518903
Marison <marison.sol@outlook.com>
2024-01-18 09:16:04 +0100

    formated edit-invoice

--- public/editInvoice.php
+++ public/editInvoice.php
@@ -4,20 +4,29 @@ require_once __DIR__ . '/../src/bootstrap.php';
 include_once '../src/fetch-customers.php';
 
 $invoice_id = $_GET['id'];
-$sql1 = "SELECT *,
-`invoices`.`id` AS `invoice_id`,
-`invoices`.`created` AS `invoice_created`,
-`invoices`.`status` AS `invoice_status` FROM invoices
-INNER JOIN invoice_status ON invoices.status=invoice_status.id
-INNER JOIN customers ON invoices.customer_id=customers.id
-INNER JOIN users ON invoices.user_id=users.id
-WHERE `invoices`.`id` = '$invoice_id'";
+$sql1 = "SELECT
+            *,
+            `invoices`.`id` AS `invoice_id`,
+            `invoices`.`created` AS `invoice_created`,
+            `invoices`.`status` AS `invoice_status` FROM invoices
+        INNER JOIN invoice_status ON invoices.status=invoice_status.id
+        INNER JOIN customers ON invoices.customer_id=customers.id
+        INNER JOIN users ON invoices.user_id=users.id
+        WHERE
+            `invoices`.`id` = '$invoice_id'";
+
 $inv_result = $con->query($sql1);
 $row = $inv_result->fetch_assoc();
 
-$fetch_items = "SELECT *, products.id AS product_nmr FROM invoice_line
-INNER JOIN products ON invoice_line.product_id=products.id
-WHERE invoice_id = '$invoice_id'";
+$fetch_items = "SELECT
+                    *,
+                    products.id AS product_nmr
+                FROM
+                    invoice_line
+                INNER JOIN products ON invoice_line.product_id=products.id
+                WHERE
+                    invoice_id = '$invoice_id'";
+                    
 $item_result = $con->query($fetch_items);
 while ($array = $item_result->fetch_assoc()) {
     $items[] = $array;
