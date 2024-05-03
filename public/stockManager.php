8532630938711c16cb9a5b5885ee935d7c01ff00
Abdalrhman10 <67341893+Abdalrhman10@users.noreply.github.com>
2024-05-03 11:41:49 +0200

    stock manager aanpassing

--- public/stockManager.php
+++ public/stockManager.php
@@ -5,7 +5,7 @@
 
       class StockManager {
         private $db;
-    
+        
         public function __construct($db) {
             $this->db = $db;
         }
@@ -13,22 +13,32 @@
         public function checkStockLevels() {
             $warnings = "";
     
-            $sql = "SELECT products.id, products.product_name, products.product_quantity, products.min_stock
-                    FROM products";
+            $sql = "SELECT product_name, product_quantity, min_stock FROM products";
             $result = $this->db->query($sql);
     
             while ($row = $result->fetch_assoc()) {
-                $product_name = $row['product_name'];
-                $product_quantity = $row['product_quantity'];
-                $min_stock = $row['min_stock'];
+                $product = $row['product_name'];
+                $stockLevel = $row['product_quantity'];
+                $minStockLevel = $row['min_stock'];
     
-                if ($product_quantity < $min_stock) {
-                    $warnings .= "Low stock for product: $product_name. Current quantity: $product_quantity. Minimum stock: $min_stock.<br>";
+                if ($stockLevel < $minStockLevel) {
+                    $warnings .= "Waarschuwing: De voorraad van $product is te laag. Huidige voorraad: $stockLevel. Minimale voorraad: $minStockLevel.<br>";
                 }
             }
     
             return $warnings;
         }
+    }
+    
+    // Maak een instantie van de StockManager-klasse
+    $stockManager = new StockManager($con);
+    
+    // Roep de methode aan om voorraadniveaus te controleren
+    $warnings = $stockManager->checkStockLevels();
+    
+    // Toon de waarschuwingen
+    if (!empty($warnings)) {
+        echo "<div>$warnings</div>";
     }
        
              ?>
