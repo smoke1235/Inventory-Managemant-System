a8e64bb9ca31070342d2bfebbc518f7f56a2e6dc
Abdalrhman10 <67341893+Abdalrhman10@users.noreply.github.com>
2024-05-13 12:29:17 +0200

    Zoeksysteem aanpassen

--- public/view-zoeken.php
+++ public/view-zoeken.php
@@ -1,10 +1,8 @@
 <?php
 require_once '../src/inc/session_check.php'; 
- view('header', ['title' =>  'Dashboard']); 
-  ?>
-<?php
+view('header', ['title' =>  'Dashboard']); 
 
-
+require_once '../src/inc/session_check.php'; 
 
 // Controleer of resultaten in de sessie zijn opgeslagen
 if(isset($_SESSION['resultaten'])){
@@ -15,13 +13,25 @@ if(isset($_SESSION['resultaten'])){
     echo "<h1>Zoekresultaten</h1>";
     echo "<table border='1'>";
     foreach ($resultaten as $tabel => $resultaat) {
-        echo "<tr><th colspan='2'>$tabel</th></tr>";
-        foreach ($resultaat as $row) {
-            echo "<tr>";
-            foreach ($row as $kolom => $waarde) {
-                echo "<td>$kolom</td><td>$waarde</td>";
+        if (!empty($resultaat)) { // Controleer of het resultaat niet leeg is
+            echo "<tr><th colspan='" . (count($resultaat[0]) + 1) . "'>$tabel</th></tr>";
+            foreach ($resultaat as $row) {
+                echo "<tr>";
+                foreach ($row as $kolom => $waarde) {
+                    echo "<td>$waarde</td>";
+                }
+                // Haal het ID op uit de rij
+                $id = $row['id'];
+                // Bewerkingslink toevoegen met het juiste ID
+                echo "<td>
+                        <a class='edit-data' href='editProduct.php?id=$id'>
+                            <ion-icon name='pencil-outline'></ion-icon>
+                        </a>
+                      </td>";
+                echo "</tr>";
             }
-            echo "</tr>";
+        } else {
+            echo "<tr><td colspan='2'>Geen resultaten gevonden voor $tabel</td></tr>";
         }
     }
     echo "</table>";
@@ -29,4 +39,7 @@ if(isset($_SESSION['resultaten'])){
     echo "Geen resultaten gevonden.";
 }
 ?>
+
+
+
 
